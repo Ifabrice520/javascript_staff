a2ee070f29c9f0b3c372a40c6e53a61ba7880cda
Ishimwe Fabrice <ishimwefabrice520@gmail.com>
2024-10-02 17:49:59 +0200

    Variable & Datatype

--- variable.js
+++ variable.js
@@ -3,3 +3,29 @@ var name =  "ishimwe";
 var age = 20;
 
 console.log(name);
+
+// if you specify name of you variable and assign it value there are no error for it
+
+carname = "volvo";
+
+console.log(carname);
+
+
+// let variable
+
+let x = 10;
+
+let y = 12;
+
+
+// const variable
+
+const fabrice = ishimwe;
+
+function ishimwe() {
+    const f_name = "ishimwe";
+    const l_name = "fabrice";
+}
+
+
+ishimwe()

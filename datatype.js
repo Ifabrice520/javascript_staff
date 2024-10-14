68b73cfe62552d9b42ea0f83e5a3de3cd9546c14
Ishimwe Fabrice <ishimwefabrice520@gmail.com>
2024-10-14 21:12:01 +0200

    primitive datatype

---
+++ datatype.js
@@ -0,0 +1,34 @@
+//primitive datatype
+
+//number
+
+var x = 10;
+
+console.log(typeof(x));
+
+
+// string
+
+var name = "ISHIMWE Fabrice"
+
+console.log(typeof(name));
+
+
+// boolean
+
+var things = true
+
+console.log(typeof(things));
+
+
+//underfined 
+
+var pink
+
+console.log(typeof(pink));
+
+
+
+
+
+

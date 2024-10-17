2daebb87f7c9bf124465f4fe21f53a5b3216f4dd
Ishimwe Fabrice <ishimwefabrice520@gmail.com>
2024-10-17 20:49:30 +0200

    js operator

---
+++ operator.js
@@ -0,0 +1,39 @@
+// assign operator used for assigning value to variable
+
+let x = 10;
+
+// addition operator used for adding two numbers
+
+let a = 23;
+
+let b = 10;
+
+let c = a + b;
+
+
+// multiplication operator which used to make product of two or more numbers
+
+let d = a * b;
+
+
+// dividing operator which used to devide two numbers
+
+let e = a / b;
+
+//remainder(modulus) operator
+
+let f = a % b;
+
+// incrementing and decrementing operotor which used to add 1 or subtract 1 on the value
+
+x++;
+
+a--;
+
+console.log(a);
+
+// comparison operator
+
+let g = 12;
+
+console.log(g === "12");
